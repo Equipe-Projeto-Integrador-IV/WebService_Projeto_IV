68e76dd117276b8cee2b51e4a23cafe7111e316a
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-05-30 12:11:42 -0300

    teste

---
+++ src/main/java/com/projetoic/senac/WebServiceProjetoIvApplication.java
@@ -0,0 +1,13 @@
+package com.projetoic.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceProjetoIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceProjetoIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoic/senac/WebServiceProjetoIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoic.senac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjetoIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
